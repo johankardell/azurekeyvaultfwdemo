6e3db050243f658e6c8fdf5b5467b4025192023c
Johan Kardell <johan.kardell@gmail.com>
2022-03-25 15:52:19 +0100

    Updates

--- keyvault.tf
+++ keyvault.tf
@@ -3,7 +3,6 @@ resource "azurerm_key_vault" "kv" {
   name                        = "kv-fw-demo-jk-2022"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
@@ -16,6 +15,12 @@ resource "azurerm_key_vault" "kv" {
 
     key_permissions = [
       "Get",
+    ]
+
+    certificate_permissions = [
+      "Get",
+      "List",
+      "Create"
     ]
 
     secret_permissions = [
@@ -39,10 +44,25 @@ resource "azurerm_key_vault" "kv" {
     ]
   }
 
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.appgw.principal_id
+
+    certificate_permissions = [
+      "Get",
+      "List"
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List"
+    ]
+  }
+
   network_acls {
     bypass         = "AzureServices"
     default_action = "Deny"
-    ip_rules       = [azurerm_public_ip.ubuntu.ip_address, "155.4.130.187/32"]
+    ip_rules       = [azurerm_public_ip.ubuntu.ip_address,  "155.4.130.187/32"]
   }
 }
 
